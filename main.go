c0ad1f6604e876d273093fca77e8dd90031e2189
Bader <alafoobader@gmail.com>
2025-08-04 00:17:09 +0300

    added username requirement

--- main.go
+++ main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
+	"time"
 )
 
 func main() {
-
-	if len(os.Args) < 2 {
+	Args := os.Args
+	if len(Args) > 2 {
 		fmt.Println("Usage: go run main.go <port>")
 		os.Exit(1)
 	}
-
-	port := fmt.Sprintf(":%s", os.Args[1])
-
+	port := ":8989"
+	if len(Args) == 2 {
+		port = ":"+Args[1]
+	}
+	
 	listener, err := net.Listen("tcp", port)
 	if err != nil {
 		fmt.Println("Error starting server:", err)
@@ -22,7 +27,7 @@ func main() {
 	}
 	defer listener.Close() // we clean the port when done with program
 
-	fmt.Println("Server listening on port 8989")
+	fmt.Println("Server listening on port" + port)
 
 	// waiting for clients
 	for {
@@ -31,12 +36,30 @@ func main() {
 			fmt.Println("Error accepting client:", err)
 			continue
 		}
-
-		go handleConnection(conn) 
+		go handleConnection(conn)
 	}
 }
 
-func handleConnection(conn net.Conn) {
+func requireName(reader *bufio.Reader, conn net.Conn) (string, error) {
+	conn.Write([]byte("[" + time.Now().Format("2006-01-02 15:04:05") + "]"))
+	conn.Write([]byte("Enter Username:"))
+	name, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("couldnt read name")
+		return "", err
+	}
+	name = strings.TrimSpace(name)
+	return name, nil
+}
+
+func handleConnection(conn net.Conn) error {
 	defer conn.Close()
-	fmt.Println("Client connected:", conn.RemoteAddr())
+	reader := bufio.NewReader(conn)
+	username, usernameError := requireName(reader, conn)
+	if usernameError != nil {
+		fmt.Println(usernameError)
+		return usernameError
+	}
+	fmt.Println("Connection Established from username: " + username)
+	return	nil
 }
